74bcd4ee0e7448fd3e6879807f2faaae2d2f3ee1
Vishal Sachan <129661155+vishalsachan123@users.noreply.github.com>
2025-03-27 22:32:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -44,4 +44,4 @@ def index():
     return render_template("myhome.html")
 
 if __name__ == "__main__":
-    socketio.run(app, host="0.0.0.0", port=5000, debug=True)
+    socketio.run(app, host="0.0.0.0", port=5000, debug=True, allow_unsafe_werkzeug=True)
